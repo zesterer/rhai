6ab837cb53ef39bbfc79b2cebfec5bd705bd3764
Lukáš Hozda <luk.hozda@gmail.com>
2017-11-24 15:10:18 +0100

    update repl examples

--- examples/repl.rs
+++ examples/repl.rs
@@ -11,12 +11,9 @@ fn showit<T: Display>(x: &mut T) -> () {
 
 fn quit() { exit(0); }
 
-pub fn main() {
-	let mut engine = Engine::new();
-	let mut scope = Scope::new();
-
-	engine.register_fn("print", showit as fn(x: &mut i32)->());
+fn register(engine: &mut Engine) {
     engine.register_fn("print", showit as fn(x: &mut i64)->());
+    engine.register_fn("print", showit as fn(x: &mut i32)->());
     engine.register_fn("print", showit as fn(x: &mut u32)->());
     engine.register_fn("print", showit as fn(x: &mut u64)->());
     engine.register_fn("print", showit as fn(x: &mut f32)->());
@@ -24,17 +21,25 @@ pub fn main() {
     engine.register_fn("print", showit as fn(x: &mut bool)->());
     engine.register_fn("print", showit as fn(x: &mut String)->());
     engine.register_fn("exit", quit);
+}
 
-	loop {
-		print!("> ");
-		let mut input = String::new();
-		stdout().flush().expect("couldn't flush stdout");
-		if let Err(e) = stdin().read_line(&mut input)  {
-			println!("input error: {}", e);
-		}
+pub fn main() {
+    let mut engine = Engine::new();
+    let mut scope = Scope::new();
 
-		if let Err(e) = engine.consume_with_scope(&mut scope, &input) {
-			println!("error: {}", e);
-		}
-	}
+    register(&mut engine);
+    engine.module_fns(register);
+
+    loop {
+        print!("> ");
+        let mut input = String::new();
+        stdout().flush().expect("couldn't flush stdout");
+        if let Err(e) = stdin().read_line(&mut input)  {
+            println!("input error: {}", e);
+        }
+
+        if let Err(e) = engine.consume_with_scope(&mut scope, &input) {
+            println!("error: {}", e);
+        }
+    }
 }
